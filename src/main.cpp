002b353374918d92e0ea7d31c8fa9e0308f09c30
Sasha <klimdos@gmail.com>
2019-05-19 22:10:38 +0300

    dht works

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,53 @@
 #include <Arduino.h>
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+#include <DHT.h>
 
+#define DHTPIN 2     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define fan 4
+
+
+int maxHum = 60;
+int maxTemp = 40;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// the setup function runs once when you press reset or power the board
+
+
+void setup() {
+  pinMode(fan, OUTPUT);
+  Serial.begin(9600); 
+  dht.begin();
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.println("On");    
-  delay(700);                  // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  Serial.println("Off");      
-  delay(200);                  // wait for a second
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  
+  if(h > maxHum || t > maxTemp) {
+      digitalWrite(fan, HIGH);
+  } else {
+     digitalWrite(fan, LOW); 
+  }
+  
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.println(" *C ");
+
 }
